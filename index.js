a0b95599c6115d0f96f58c53ab368452ae989919
unknown <24abdullahalmasum@gmail.com>
2023-05-21 21:44:30 +0600

    fixing issus

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ async function run() {
         res.send(result)
       })
 
-      // to see vew toys details
+      // to see vew the  toys vew details
       app.get('/alltoys/toys/:id', async (req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id)}
